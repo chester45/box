dbcbe8b5cdcfe544cbd934ab419676621dbbb306
Wojciech Serek <wojciech.serek@tieto.com>
2018-08-17 14:38:29 +0200

    Ino: Add status LED, modify sleep
    Change-Id: I1ecaa404cfd3f53e4849436affc2037c16ce886c
    Note: LED is not flashing during sleep
    TODO: Add conditions before going to sleep

--- box.ino
+++ box.ino
@@ -1,11 +1,23 @@
 #include <avr/sleep.h>
 #include <Arduino.h>
+#include <Servo.h>
 #include "Box.h"
 #include "log.h"
 #include "TimerObjectManager.h"
 
+#define STATUS_LED_PIN  (13)
+
+#define UNUSED_PARAM(x) (void)(x);
+
+#define LED_CLEAR(pin)  digitalWrite(pin, 0)
+#define LED_SET(pin)    digitalWrite(pin, 1)
+#define LED_TOGGLE(pin) digitalWrite(pin, !digitalRead(pin))
+
 TimerObjectManager *TimerManager = TimerObjectManager::GetManager();
-Box::Box box(DEFAULT_USER_SWITCH);
+Box::Box box(DEFAULT_USER_SWITCH, DEFAULT_COVER_SERVO_PIN, DEFAULT_STICK_SERVO_PIN);
+uint8_t StatusLedTimerIdx = TimerObjectManager::INVALID_TIMER_IDX;
+Servo servo;
+
 
 static void write_log(char *buf)
 {
@@ -14,8 +26,13 @@ static void write_log(char *buf)
 
 static void interrputCallback(void)
 {
-    sleep_disable();
-    detachInterrupt(digitalPinToInterrupt(box.GetSwitchPin()));
+}
+
+void StatusLedTimerCallback(void *param)
+{
+    UNUSED_PARAM(param)
+
+    LED_TOGGLE(STATUS_LED_PIN);
 }
 
 static void sleepSetup()
@@ -23,29 +40,34 @@ static void sleepSetup()
     LOG("zzzZZZzzz...\n");
     Serial.flush();                 // flush serial port before sleep
     cli();
+    set_sleep_mode(SLEEP_MODE_STANDBY);
     sleep_enable();
     attachInterrupt(digitalPinToInterrupt(box.GetSwitchPin()),
                     interrputCallback, LOW);    // user switch can wakeup
-    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
     sei();
-    sleep_cpu();
-    LOG("Active again !\n");
+    sleep_mode();
+    sleep_disable();
+    detachInterrupt(digitalPinToInterrupt(box.GetSwitchPin()));
 }
 
 void setup()
 {
     Serial.begin(9600);
     log_initialize(write_log);
+    pinMode(STATUS_LED_PIN, OUTPUT);
+    servo.attach(9);
     LOG("Setup complete\n");
+    StatusLedTimerIdx = TimerManager->CreateTimer(1000, NULL, StatusLedTimerCallback, false);
+    if (StatusLedTimerIdx != TimerObjectManager::INVALID_TIMER_IDX)
+    {
+        TimerManager->StartTimer(StatusLedTimerIdx);
+    }
 }
 
 void loop()
 {
     box.Check();
     TimerManager->UpdateTimers();
-    if (box.IsIdle())
-    {
-        LOG("Going to sleep\n");
-        sleepSetup();
-    }
+    sleepSetup();
+    LOG("Active again !\n");
 }
